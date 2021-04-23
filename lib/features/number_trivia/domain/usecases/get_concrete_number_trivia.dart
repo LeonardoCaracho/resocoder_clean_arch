c6e36decfc9d572fa14580835229f7f9bc6b3924
Leonardo <leeo.apc@gmail.com>
2021-04-23 17:33:52 -0300

    entity and usecases

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:resocoder_clean_arch/core/error/failures.dart';
+import 'package:resocoder_clean_arch/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:resocoder_clean_arch/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({@required int number}) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
+}
---
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:mockito/mockito.dart';
+import 'package:resocoder_clean_arch/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:resocoder_clean_arch/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:resocoder_clean_arch/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  GetConcreteNumberTrivia usecase;
+  MockNumberTriviaRepository repository;
+
+  setUp(() {
+    repository = new MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(repository);
+  });
+
+  final tNumber = 1;
+  final tNumberTrivia = NumberTrivia(number: 1, text: 'test');
+
+  test('should get trivia for the number from the repository', () async {
+    when(repository.getConcreteNumberTrivia(any))
+        .thenAnswer((_) async => Right(tNumberTrivia));
+
+    final result = await usecase.execute(number: tNumber);
+
+    expect(result, Right(tNumberTrivia));
+    verify(repository.getConcreteNumberTrivia(tNumber)).called(1);
+    verifyNoMoreInteractions(repository);
+  });
+}
